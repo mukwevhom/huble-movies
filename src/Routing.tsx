d812151f385ec2367de85a2cc565e61433795014
Murendeni Mukwevho <murendenim@girlcode.co.za>
2024-03-05 19:35:55 +0200

    Added movie & search route

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
+import Search from "./routes/Search";
+import Movie from "./routes/Movie";
 
 export default function Routing () {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/movie" element={<Movie />} />
             </Routes>
         </BrowserRouter>
     )
